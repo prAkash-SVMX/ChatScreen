c2abb1e47509c5eb6bb903db208b8860edf0d508
Prakash Narayan Shukla <prakashnarayan.shukla@servicemax.com>
2023-11-10 18:00:15 +0530

    feat: documented code

--- src/ChatComponent.js
+++ src/ChatComponent.js
@@ -1,93 +1,110 @@
-import React, { Component } from 'react';
-import Icon from 'react-native-ico-material-design';
+import React, { Component } from "react";
+import Icon from "react-native-ico-material-design";
 import {
   KeyboardAvoidingView,
   StyleSheet,
   FlatList,
-  Image,
   Text,
   TextInput,
   Platform,
   View,
   Modal,
-} from 'react-native';
-import Color from './color';
-import FontSize from './fontsize';
+} from "react-native";
+import Color from "./color";
+import FontSize from "./fontsize";
 class ChatComponent extends Component {
   constructor(props: Object) {
     super(props);
 
+    // Initial state of the component
     this.state = {
       textInputHeight: 40,
-      inputValue: '',
+      inputValue: "",
       refreshing: false,
       modelVisible: false,
-      data: [
-        {
-          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-          title: 'First Item',
-        },
-        {
-          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-          title: 'Second Item',
-        },
-        {
-          id: '58694a0f-3da1-471f-bd96-145571e29d72',
-          title: 'Third Item',
-        },
-      ],
+      data: [], // Array to store chat data
     };
   }
 
+  // Function to handle text input submission
   _onSubmitEditing = () => {
     const newInputValue = this.state.inputValue.trim();
     if (newInputValue) {
       const newData = this.state.data;
+      // Adding a new chat message to the beginning of the data array
       newData.unshift({ id: Math.random(), title: newInputValue });
-      this.setState({ data: newData, inputValue: '', });
+      this.setState({ data: newData, inputValue: "" });
     }
   };
+
+  // Function to render a single row (message) in the chat
   _renderRow = (id, sender, message) => {
-    return <MessageCell key={`cell-${id}`} sender={sender} message={message} />;
+    const chatViewColor =
+      this.props.chatViewColor || styles.messageCell.backgroundColor;
+    return (
+      <MessageCell
+        chatViewColor={chatViewColor}
+        key={`cell-${id}`}
+        sender={sender}
+        message={message}
+      />
+    );
   };
-  setVisible = visibility => {
+
+  // Function to toggle the visibility of the modal
+  setVisible = (visibility) => {
     const visible = this.state.modelVisible;
     this.setState({ ...this.state, modelVisible: !visible });
   };
-  handleKeyDown = e => {
-    if (e.nativeEvent.key == 'Enter') {
+
+  // Function to handle the "Enter" key press event
+  handleKeyDown = (e) => {
+    if (e.nativeEvent.key == "Enter") {
       this._onSubmitEditing();
     }
   };
+
+  // Render method for the ChatComponent
   render() {
     const visible = this.state.modelVisible;
-    debugger
-    const headerBackgroundColor=this.props.headerBackgroundColor || styles.headerContent.backgroundColor;
-    const headerTintColor= this.props.headerTintColor || styles.headerText.color;
+    const headerBackgroundColor =
+      this.props.headerBackgroundColor || styles.headerContent.backgroundColor;
+    const headerTintColor =
+      this.props.headerTintColor || styles.headerText.color;
+    const headerText = this.props.headerText || "Chat";
+
+    // Content when the modal is not visible
     let withoutModel = (
       <View
         style={{
           flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Text onPress={this.setVisible}>visible</Text>
       </View>
     );
+    //view for android and windows
     let withModel = (
       <Modal animationType="slide" transparent={true} visible={visible}>
         <View style={styles.container}>
+          {/* Header of the modal */}
           <View
-            style={[styles.headerContent,{backgroundColor:headerBackgroundColor}]}>
+            style={[
+              styles.headerContent,
+              { backgroundColor: headerBackgroundColor },
+            ]}
+          >
             <View>
-              <>
-                {/* {//<Image source={require('./user.png')}></Image>
-        } */}
-                <View style={{ marginLeft: '65%' }}>
-                  <Text style={[styles.headerText,{color:headerTintColor}]}>Text</Text>
-                </View>
-              </>
+              {/* Header text */}
+              <View style={{ marginLeft: "65%" }}>
+                <Text style={[styles.headerText, { color: headerTintColor }]}>
+                  {headerText}
+                </Text>
+              </View>
             </View>
+            {/* Close button */}
             <Icon
               name="close-button"
               height="16"
@@ -96,19 +113,23 @@ class ChatComponent extends Component {
               onPress={this.setVisible}
             />
           </View>
+
+          {/* Content of the modal */}
           <View style={styles.contentView}>
             {console.log(this.state.data)}
+            {/* FlatList to render chat messages */}
             <FlatList
               data={this.state.data}
               renderItem={({ item }) =>
                 this._renderRow(item.id, true, item.title)
               }
-              keyExtractor={item => item.id}
+              keyExtractor={(item) => item.id}
               inverted={true}
             />
           </View>
-          <View
-            style={styles.inputContainer}>
+
+          {/* Input container for typing and sending messages */}
+          <View style={styles.inputContainer}>
             <TextInput
               style={[
                 styles.input,
@@ -118,7 +139,7 @@ class ChatComponent extends Component {
                     40,
                     this.state.textInputHeight < 180
                       ? this.state.textInputHeight
-                      : 180,
+                      : 180
                   ),
                 },
               ]}
@@ -132,12 +153,12 @@ class ChatComponent extends Component {
               returnKeyType="done"
               onKeyPress={this.handleKeyDown}
               enablesReturnKeyAutomatically={true}
-              onContentSizeChange={event => {
+              onContentSizeChange={(event) => {
                 this.setState({
                   textInputHeight: event.nativeEvent.contentSize.height,
                 });
               }}
-              onChangeText={text => {
+              onChangeText={(text) => {
                 this.setState({ inputValue: text });
               }}
             />
@@ -154,23 +175,37 @@ class ChatComponent extends Component {
         </View>
       </Modal>
     );
+    // Determine the content based on the platform and modal visibility
     let content = this.state.modelVisible === true ? withModel : withoutModel;
-    if (Platform.OS === 'ios' && this.state.modelVisible) {
+
+    // Return the content for ios
+    if (Platform.OS === "ios" && this.state.modelVisible) {
       return (
         <Modal animationType="slide" transparent={true} visible={visible}>
+          {/* KeyboardAvoidingView for iOS to adjust the view when the keyboard is displayed */}
           <KeyboardAvoidingView
             behavior="padding"
-            style={styles.KeyboardAvoidingView}>
+            style={styles.KeyboardAvoidingView}
+          >
             <View style={styles.container}>
+              {/* Header of the modal */}
               <View
-                style={[styles.headerContent,{backgroundColor:headerBackgroundColor}]}>
+                style={[
+                  styles.headerContent,
+                  { backgroundColor: headerBackgroundColor },
+                ]}
+              >
                 <View>
-
-                  <View style={{ marginLeft: '65%' }}>
-                    <Text style={[styles.headerText,{color:headerTintColor}]}>Text</Text>
+                  {/* Header text */}
+                  <View style={{ marginLeft: "65%" }}>
+                    <Text
+                      style={[styles.headerText, { color: headerTintColor }]}
+                    >
+                      {headerText}
+                    </Text>
                   </View>
-
                 </View>
+                {/* Close button */}
                 <Icon
                   name="close-button"
                   height="16"
@@ -179,19 +214,23 @@ class ChatComponent extends Component {
                   onPress={this.setVisible}
                 />
               </View>
+
+              {/* Content of the modal */}
               <View style={styles.contentView}>
                 {console.log(this.state.data)}
+                {/* FlatList to render chat messages */}
                 <FlatList
                   data={this.state.data}
                   renderItem={({ item }) =>
                     this._renderRow(item.id, true, item.title)
                   }
-                  keyExtractor={item => item.id}
+                  keyExtractor={(item) => item.id}
                   inverted={true}
                 />
               </View>
-              <View
-                style={styles.inputContainer}>
+
+              {/* Input container for typing and sending messages */}
+              <View style={styles.inputContainer}>
                 <TextInput
                   style={[
                     styles.input,
@@ -201,7 +240,7 @@ class ChatComponent extends Component {
                         40,
                         this.state.textInputHeight < 180
                           ? this.state.textInputHeight
-                          : 180,
+                          : 180
                       ),
                     },
                   ]}
@@ -215,12 +254,12 @@ class ChatComponent extends Component {
                   returnKeyType="done"
                   onKeyPress={this.handleKeyDown}
                   enablesReturnKeyAutomatically={true}
-                  onContentSizeChange={event => {
+                  onContentSizeChange={(event) => {
                     this.setState({
                       textInputHeight: event.nativeEvent.contentSize.height,
                     });
                   }}
-                  onChangeText={text => {
+                  onChangeText={(text) => {
                     this.setState({ inputValue: text });
                   }}
                 />
@@ -243,66 +282,57 @@ class ChatComponent extends Component {
     }
   }
 }
+
+// MessageCell component for rendering individual chat messages
 class MessageCell extends Component {
   render() {
-    let { sender, message } = this.props;
-
-    let differentStyle = {};
-    if (sender) {
-      differentStyle = {
-        flexDirection: 'row-reverse',
-        backgroundColor: '#0078fe',
-      };
-    } else {
-      differentStyle = {
-        flexDirection: 'row',
-        backgroundColor: '#0078fe',
-      };
-    }
+    let { chatViewColor, message } = this.props;
     return (
-      <View
-        style={styles.messageCell}>
-        <Text style={styles.messageCellText}>{message}</Text>
-        <View style={styles.rightArrow}></View>
+      <View style={[styles.messageCell, { backgroundColor: chatViewColor }]}>
+        <Text style={[styles.messageCellText]}>{message}</Text>
+        <View
+          style={[styles.rightArrow, { backgroundColor: chatViewColor }]}
+        ></View>
         <View style={styles.rightArrowOverlap}></View>
       </View>
     );
   }
 }
 
+// Styles for the components
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
+    justifyContent: "flex-start",
+    alignItems: "stretch",
     backgroundColor: Color.BackgroundGrey,
   },
   KeyboardAvoidingView: {
     flex: 1,
   },
   inputContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingBottom: 30,
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   headerContent: {
     paddingTop: 50,
-    flexDirection: 'row',
-    height: '30',
-    alignItems: 'center',
-    width: '100%',
-    justifyContent: 'space-between',
-    backgroundColor: '#93cfdb',
+    flexDirection: "row",
+    height: "30",
+    alignItems: "center",
+    width: "100%",
+    justifyContent: "space-between",
+    backgroundColor: "#93cfdb",
   },
   headerText: {
     fontSize: FontSize.Content,
     fontWeight: "bold",
-    color:'#ffff'
+    color: "#ffff",
   },
   bottomToolBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     borderTopWidth: 1,
     borderTopColor: Color.LittleGrey,
   },
@@ -318,20 +348,20 @@ const styles = StyleSheet.create({
     flex: 1,
     color: Color.Black,
     fontSize: FontSize.Main,
-    backgroundColor: '#d7dcde',
+    backgroundColor: "#d7dcde",
     borderRadius: 10,
-    textAlign: 'center',
+    textAlign: "center",
     padding: 10,
   },
   messageCell: {
-    backgroundColor: '#0078fe',
+    backgroundColor: "#0078fe",
     padding: 10,
-    marginLeft: '45%',
+    marginLeft: "45%",
     borderRadius: 5,
     marginTop: 5,
-    marginRight: '5%',
-    maxWidth: '50%',
-    alignSelf: 'flex-end',
+    marginRight: "5%",
+    maxWidth: "50%",
+    alignSelf: "flex-end",
     borderRadius: 20,
   },
   messageCellText: {
@@ -345,8 +375,8 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   rightArrow: {
-    position: 'absolute',
-    backgroundColor: '#0078fe',
+    position: "absolute",
+    backgroundColor: "#0078fe",
     //backgroundColor:"red",
     width: 20,
     height: 25,
@@ -356,8 +386,8 @@ const styles = StyleSheet.create({
   },
 
   rightArrowOverlap: {
-    position: 'absolute',
-    backgroundColor: '#eeeeee',
+    position: "absolute",
+    backgroundColor: "#eeeeee",
     //backgroundColor:"green",
     width: 20,
     height: 35,
@@ -375,19 +405,19 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     padding: 4,
     paddingHorizontal: 8,
-    overflow: 'hidden',
+    overflow: "hidden",
     flex: 1,
     margin: 5,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   chatSendContainer: {
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
     flex: 2,
   },
 
   endBlankBlock: {
-    position: 'absolute',
-    backgroundColor: '#eeeeee',
+    position: "absolute",
+    backgroundColor: "#eeeeee",
     //backgroundColor:"green",
     width: 20,
     height: 35,
